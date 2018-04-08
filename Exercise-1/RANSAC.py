e90fd1bbd3cbdcc07a26d9cde4dc2af4a840636f
Trevor Kinney <trevorkinney@hotmail.com>
2018-04-08 00:13:30 -0600

    Got file from vm that is most up to date version.

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -6,9 +6,35 @@ cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 
 # Voxel Grid filter
+vox = cloud.make_voxel_grid_filter()
 
+# Need to test different values
+# Value of 1 implies one cubic meter in volume. Should start small and then go up
+LEAF_SIZE = 0.01
+vox.set_leaf_size(LEAF_SIZE,LEAF_SIZE,LEAF_SIZE)
+
+# Call the filer function to obtain the resultant downsampled point cloud
+cloud_filtered = vox.filter()
+filename = 'voxel_downsampled.pcd'
+pcl.save(cloud_filtered, filename)
 
 # PassThrough filter
+# Create a passthrough filter object
+passthrough = cloud_filtered.make_passthrough_filter()
+
+# Assign axis and range to the passthrough filter object
+filter_axis = 'z'
+passthrough
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits(axis_min, axis_max)
+
+# Finally use the filter function to obtain the resultant point cloud
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered,filename)
+
 
 
 # RANSAC plane segmentation
@@ -24,5 +50,3 @@ cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 
 # Save pcd for tabletop objects
-
-
