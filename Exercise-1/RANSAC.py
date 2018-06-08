91ac19903434c729e925a16f643602207aea2bc3
Trevor <trevorkinney@hotmail.com>
2018-06-08 11:34:09 -0700

    DId exercise one. Working on DBSCAN part now

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -18,6 +18,8 @@ cloud_filtered = vox.filter()
 filename = 'voxel_downsampled.pcd'
 pcl.save(cloud_filtered, filename)
 
+# ------------------------------------------------
+
 # PassThrough filter
 # Create a passthrough filter object
 passthrough = cloud_filtered.make_passthrough_filter()
@@ -35,18 +37,45 @@ cloud_filtered = passthrough.filter()
 filename = 'pass_through_filtered.pcd'
 pcl.save(cloud_filtered,filename)
 
-
+# ------------------------------------------------
 
 # RANSAC plane segmentation
+# Create a segmentation object
+seg = cloud_filtered.make_segmenter()
 
+# Set the model to be fit
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+
+# Max distance for a point to be considered fitting the model
+max_distance = 0.01
+seg.set_distance_threshold(max_distance)
+inliers, coefficients = seg.segment()
 
 # Extract inliers
+# Negative flag basically means if you want the positive or negative of the shape extraction
+extracted_inliers = cloud_filtered.extract(inliers, negative=True)
+filename = 'extracted_inliers.pcd'
+pcl.save(extracted_inliers, filename)
 
-# Save pcd for table
-# pcl.save(cloud, filename)
-
+# ------------------------------------------------
 
 # Extract outliers
+# First create filter object
+# outlier_filter = cloud_filtered.make_statistical_outlier_filter()
 
+# # Set the number of neighboring points to analyze for a given point
+# outlier_filter.set_mean_k(50)
 
-# Save pcd for tabletop objects
+# # Set threshold scale factor
+# x = 1.0
+
+# # Any point with a mean distance larer than lobal will be considered an outlier
+# outlier_filter.set_std_dev_mul_thresh(x)
+
+# # Call filter function again to apply outlier filer to pcd
+# cloud_filtered = outlier_filter.filter()
+
+# # Save pcd for tabletop object
+# filename = 'extracted_outliers'
+# pcl.save(extracted_inliers, filename)
