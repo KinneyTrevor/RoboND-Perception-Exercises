09a855ed861ed6b02edda534c3ea1db25d5db66c
Trevor <trevorkinney@hotmail.com>
2018-06-21 21:01:43 -0700

    Got exercise three done. It's predicting but the predictions are pretty trash

--- Exercise-3/sensor_stick/scripts/segmentation.py
+++ Exercise-3/sensor_stick/scripts/segmentation.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 
-# Import modules
-from pcl_helper import *
-import pcl
+import numpy as np
+import sklearn
+from sklearn.preprocessing import LabelEncoder
 
-# TODO: Define functions as required
+import pickle
+
+from sensor_stick.srv import GetNormals
+from sensor_stick.features import compute_color_histograms
+from sensor_stick.features import compute_normal_histograms
+from visualization_msgs.msg import Marker
+
+from sensor_stick.marker_tools import *
+from sensor_stick.msg import DetectedObjectsArray
+from sensor_stick.msg import DetectedObject
+from sensor_stick.pcl_helper import *
+
+
+
+def get_normals(cloud):
+    get_normals_prox = rospy.ServiceProxy('/feature_extractor/get_normals', GetNormals)
+    return get_normals_prox(cloud).cluster
+
+
+
 
 # Callback function for your Point Cloud Subscriber
 def pcl_callback(pcl_msg):
@@ -64,7 +83,7 @@ def pcl_callback(pcl_msg):
     # extracted_cloud_objects_inliers = objects_outlier_filter.filter()
 
 
-    # TODO: Euclidean Clustering
+    # Euclidean Clustering
     # PCL Euclidiean Clustering only supports k-d trees
     # Need to remove color data
     white_cloud = XYZRGB_to_XYZ(extracted_cloud_objects_inliers)
@@ -87,7 +106,7 @@ def pcl_callback(pcl_msg):
     # Get dem indices
     cluster_indices = color_extraction_object.Extract()
 
-    # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+    # Create Cluster-Mask Point Cloud to visualize each cluster separately
     # Give each cluster a color
     cluster_color = get_color_list(len(cluster_indices))
 
@@ -109,25 +128,86 @@ def pcl_callback(pcl_msg):
     ros_cloud_table = pcl_to_ros(extracted_cloud_table_inliers)
     ros_cluster_cloud = pcl_to_ros(cluster_cloud)
 
-    # TODO: Publish ROS messages
+    # Publish ROS messages
     pcl_objects_pub.publish(ros_cloud_objects)
     pcl_table_pub.publish(ros_cloud_table)
     pcl_color_cluster_pub.publish(ros_cluster_cloud)
 
+# Exercise-3 TODOs: 
+
+    # Classify the clusters! (loop through each detected cluster one at a time)
+    detected_objects_labels = []
+    detected_objects = []
+    labeled_features = []
+
+    for index, pts_list in enumerate(cluster_indices):
+        
+        # Grab the points for the cluster
+        pcl_cluster = extracted_cloud_objects_inliers.extract(pts_list)
+
+        # Convert from PCL to ROS
+        ros_cluster = pcl_to_ros(pcl_cluster)
+
+        # Compute the associated feature vector
+        models = [\
+	       'beer',
+	       'bowl',
+	       'create',
+	       'disk_part',
+	       'hammer',
+	       'plastic_cup',
+	       'soda_can']
+
+
+        chists = compute_color_histograms(ros_cluster, using_hsv=True)
+        normals = get_normals(ros_cluster)
+        nhists = compute_normal_histograms(normals)
+        feature = np.concatenate((chists, nhists))
+        #labeled_features.append([feature, model_name])
+
+        # Make the prediction
+        prediction = clf.predict(scaler.transform(feature.reshape(1,-1)))
+        label = encoder.inverse_transform(prediction)[0]
+        detected_objects_labels.append(label)
+
+        # Publish a label into RViz
+        label_pos = list(white_cloud[pts_list[0]])
+        label_pos[2] += .4
+        object_markers_pub.publish(make_label(label, label_pos, index))
+
+        # Add the detected object to the list of detected objects.
+        do = DetectedObject()
+        do.label = label
+        do.cloud = ros_cluster
+        detected_objects.append(do)
+            
+    # Publish the list of detected objects
+    rospy.loginfo('Detected {} objects: {}'.format(len(detected_objects_labels), detected_objects_labels))
+    detected_objects_pub.publish(detected_objects)
 
 if __name__ == '__main__':
 
-    # TODO: ROS node initialization
+    # ROS node initialization
     rospy.init_node('clustering', anonymous=True)
 
-    # TODO: Create Subscribers
+    # Create Subscribers
     pcl_sub = rospy.Subscriber("/sensor_stick/point_cloud", pc2.PointCloud2, pcl_callback, queue_size=1)
     
-    # TODO: Create Publishers
+    # Create Publisher
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
     pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
     pcl_color_cluster_pub = rospy.Publisher("/pcl_clusters", PointCloud2, queue_size=1)
+    detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size=1)
+    object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size=1)
+
+    # Load models from disk
+    model = pickle.load(open('model.sav', 'rb'))
+    clf = model['classifier']
+    encoder = LabelEncoder()
+    encoder.classes_ = model['classes']
+    scaler = model['scaler']
     
+
     # Initialize color_list
     get_color_list.color_list = []
 
